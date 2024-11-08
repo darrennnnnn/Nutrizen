568c784ca97590821644a2f7e8aae1454169ecae
Darren <darrennathaneil@gmail.com>
2024-11-08 16:43:59 +0700

    add google provider

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,12 +1,11 @@
 import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
 
-// Notice this is only an object, not a full Auth.js instance
 export default {
-    providers: [GitHub],
+    providers: [GitHub, Google],
     callbacks: {
         authorized: async ({ auth }) => {
-            // Logged in users are authenticated, otherwise redirect to login page
             return !!auth;
         },
     },
